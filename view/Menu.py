29709e086f64c29df81bef2bd2884c63b11912f9
Mariia <maria.gadova@gmail.com>
2023-09-23 12:50:03 +0300

    Upd3

--- view/Menu.py
+++ view/Menu.py
@@ -1,4 +1,4 @@
-main_menu = '''Enter 1 - 4 to select :
+main_menu = '''Enter 1 - 5 to select :
     1. Make a new note 
     2. Change note
     3. Find note
@@ -7,4 +7,4 @@ main_menu = '''Enter 1 - 4 to select :
     '''
     
 menu_selection = "Selected : "
-error_msg = "Invalid selection, choose 1-4"
+error_msg = "Invalid selection, choose 1-5"
